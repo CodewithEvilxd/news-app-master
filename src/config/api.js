9ea311325df1872d8a75d453d7db58a381e124c6
nishantgauravhere <126612139+CodewithEvilxd@users.noreply.github.com>
2024-04-07 15:17:28 +0530

    first commit

---
+++ src/config/api.js
@@ -0,0 +1,7 @@
+const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
+const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
+const API_KEY = "ce9afeb4bf76148be5f6ddd3ac899314";
+export const endpointPath = (country, category) =>
+  `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
+export const endpointSearch = (searchQuery) =>
+  `${API_SEARCH_DOMAIN}${searchQuery}&lang=en&apikey=${API_KEY}`;
